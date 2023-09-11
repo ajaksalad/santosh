6ae74f2d3a7d7aeb6fd39a6cb39a4f09e08c66fb
ajaksalad <ajaksalad@hotmail.com>
2023-09-10 17:48:53 -0700

    initial commit

---
+++ santo.sh
@@ -0,0 +1,199 @@
+mexican santosh? juantosh.
+canadian santosh? torontosh.
+hft santosh? quantosh.
+nutty right-wing santosh? qanontosh.
+basketball santosh? lebrontosh.
+flatbread santosh? naantosh.
+chinese nationalist santosh? hantosh.
+fertilizer santosh? guantosh.
+boomer santosh? get-off-my-lawntosh.
+god of time santosh? chrontosh.
+electrolyte santosh? brawndosh.
+mafia santosh? dontosh.
+old politician santosh? gerontosh.
+baby deer santosh? fawntosh.
+tired sleepy santosh? yawntosh.
+hair stylist santosh? salontosh.
+chess piece santosh? pawntosh.
+fashionable italian santosh? milantosh.
+spooky scary halloween santosh? hauntosh.
+flakey buttery delicious santosh? croissantosh.
+robert santosh? plantosh.
+bash script santosh? santo.sh.
+insect santosh? antosh.
+custard santosh? flantosh.
+seafood santosh? prawntosh.
+middle eastern santosh? lebanontosh.
+genetically modified food santosh? monsantosh.
+facebook matketplace santosh? contosh.
+barbie santosh? blondetosh.
+british cartoon sheep santosh? shauntosh.
+nuclear santosh? nuketosh.
+arabic ruler santosh? sultantosh.
+indian god santosh? bhagavantosh.
+buff santosh? brawntosh.
+stealing varun’s project partner? chandtosh.
+largest port in the gulf of mexico for receiving deepwater oil production santosh? port fourchontosh.
+eating chinese food santosh? won-ton-tosh.
+salsa dancing santosh? cha-cha-chantosh.
+not saying around anymore santosh? abscondtosh.
+uf programming lecturer? amantosh.
+upmarket catering business santosh? gourmondosh.
+libertarian santosh? rontosh.
+performance improvement plan santosh? amazontosh.
+japanese electronics and heavy industry conglomerate santosh? santoshiba.
+massive annual midwest airplane show and enthusiast gathering? santoshkosh.
+tadpole santosh? spawntosh.
+philly santosh? jawntosh.
+missing person santosh? gonetosh.
+korean black bean noodle santosh? jjajangmyeontosh.
+bird santosh? swantosh.
+top brass in the department of defense santosh? pentagontosh.
+a new era of santosh? dawntosh.
+kawaii neko santosh? nyantosh.
+noble gases santosh? neontosh. argontosh. kryptontosh. xenontosh. radontosh.
+large herbivorous dinosaur santosh? brontoshaurus.
+dark lord of mordor santosh? saurontosh.
+massive western chinese city, one of the four great ancient capitals of china santosh? xi-antosh.
+never happened santosh? tiananmentosh.
+island province of china santosh? taiwantosh.
+costco instant noodles santosh? a-sha tainantosh.
+former new york yankees player santosh?  santosheira, mark.
+exploding american compact automobile santosh? ford pintosh.
+you can do better santosh? c’montosh.
+group card game santosh? avalontosh.
+refrigerant santosh? freontosh.
+millenial breakfast of choice? avocado tosht.
+lower class assistant santosh? peontosh.
+perfect game santosh? diego germantosh.
+nepomniachtchi santosh? iantosh.
+mr. lecun, head of facebook ai research santosh? yanntosh.
+4chan santosh? anontosh.
+dogecoin santosh? elontosh.
+small korean side dishes santosh? banchantosh.
+stinky taiwanese santosh? sumanthosh.
+santosh through the ages? eontosh.
+misfolded protein santosh? priontosh.
+alert light used in an industrial setting santosh? andontosh.
+cut in half santosh? sawntosh.
+nigerian beans in coconut sauce santosh? frejontosh.
+equal chess game santosh? drawntosh.
+my fellow americans santosh? obamtosh.
+turkish basketball player santosh? cedi osmantosh.
+muslim santosh? pakistantosh.
+borat santosh? kazakhstantosh.
+uranium enriching santosh? irantosh.
+andrew’s brother hebrew name santosh? sha’anantosh.
+unfriendly towards armenians santosh? azerbaijantosh.
+large body of water in the east cascade mountains of washington santosh? lake chelantosh.
+apple jacks santosh? dawinnamontosh.
+grassy santosh? awntosh.
+$1 cheese stick santosh? wawantosh.
+corn native american santosh? squantosh.
+dc chess park santosh? dupontosh.
+board the trolley santosh? hop-on-tosh.
+small western african country santosh? gabontosh.
+spanish chess player santosh? david antontosh guijarro.
+scrum master santosh? kanbantosh.
+chinese woman warrior santosh? mulantosh.
+islamic book santosh? qurantosh.
+elizabeth warren santosh? pocohantosh.
+expensive spice santosh? saffrontosh.
+jedi santosh? obi-wantosh.
+show-off santosh? flauntosh.
+roadman santosh? wagwantosh.
+really talented without practice santosh? savantosh.
+using a power button on a powered-off computer? ontosh.
+online academy santosh? khantosh.
+after school math tutoring santosh? kumontosh.
+1/0 santosh? NANtosh.
+third best ever dutch chess player santosh? jantosh timmantosh.
+poseidon santosh? god of the santocean.
+place of torture santosh? guantanamosh.
+sweet chocolate confection santosh? bonbontosh.
+fancy french mustard santosh? grey poupontosh.
+super tired santosh? doggonetosh.
+a man; a plan; a canal: panamantosh. 
+jewsish jelly donut santosh? sufgantosh.
+multilingual santosh? babylontosh.
+mosque leader santosh? imamtosh.
+tropical tree part santosh? palm frondtosh.
+herbal santosh? tarragontosh.
+catfished santosh? led-ontosh.
+usa ambassador to belize santosh? michelle kwantosh. 
+bullet chess trick santosh? lefongtosh.
+90s genocide santosh? rwandtosh.
+heat resistant mineral lining santosh? zircontosh.
+mathematician santosh? fermantosh.
+gamer snack santosh? doritosh.
+lost city santosh? atlantosh.
+mario’s friend santosh? santoshi.
+musical santosh? do re-mi-fa-sol-lantosh.
+florida state song santosh? suwantoshee river.
+aztec capital santosh? tenochtitlantosh.
+glitchy chess move santosh? en passantosh.
+scrumptious thai curry santosh? massamantosh.
+boisterous laugh santosh? guffawntosh.
+betel leaf santosh? paantosh.
+pan-european santosh? esperantosh.
+soy sauce santosh? kikomantosh.
+backwards santosh? tirumalantosh.
+black panther santosh? wakandtosh.
+pumpkin seed santosh? pepitantosh.
+highest singing voice santosh? soprantosh.
+ice cream santosh? gelatosh. 
+first prime minister of israel and namesake of tel-aviv's major airport santosh? ben-guriontosh. 
+french army leader and conqueror santosh? napoleontosh.
+mild, green, mexican chili pepper santosh? poblantosh.
+common hash algorithm designed by the national security agency santosh? SHAntosh 256.
+hungarian prime minister santosh? viktor orbántosh.
+canadian queen of power ballads santosh? celine diontosh.
+money saving santosh? coupontosh.
+mc hammer santosh? cantosh this.
+if he weren't smart santosh? morontosh.
+jewish headgear santosh? kippahntosh.
+thick wheat flour noodle santosh? udontosh.
+police bodycam and taser manufacturer santosh? axontosh.
+karate kid santosh? wax-ontosh, wax-offtosh.
+african american vernacular english santosh? ebontosh.
+dead meat santosh? carriontosh.
+spicy indian rice dish santosh? biryantosh.
+antisemitic russian genocide santosh? pogromtosh.
+inside-out santosh? bukkapantosh.
+nutty santosh? pecantosh.
+hot santosh? picantosh. really hot santosh? santosh.
+shellshocked vet santosh? ‘namtosh. 
+prickly tropical fruit santosh? rambutantosh.
+sister santosh? ramyantosh.
+santosh santosh? santosh.
+my favorite one santosh? santitosh.
+apple santosh? macintosh.
+chickpea santosh? garbanzosh. 
+right wing indian santosh? vivek ramaswamtosh.
+meta llm santosh? llamantosh.
+supermarket santosh? walmartosh.
+thai curry santosh? panangtosh.
+crab santosh? clawntosh.
+german consumer product manufacturer famed for their industrial design santosh? brauntosh.
+donald trump santosh? magantosh.
+goes with lime in mexican cooking santosh? cilantrosh. 
+monkey god santosh? hanumantosh.
+affluent intellectual santosh? intelligensiantosh.
+relay racing santosh? batontosh.
+taiwanese tea santosh? bobantosh.
+eastern mediterranean santosh? levantosh.
+health food santosh? quinoantosh.
+eggplant dip santosh? babasantoush.
+sausage and beer santosh? haramtosh.
+cuda gpu santosh? nvidiantosh.
+german enlightenment philosopher santosh? immanuel kantosh. 
+buzz lightyear santosh? to infinity and beyondtosh.
+
+
+[push me
+and then just touch me
+until I can get my
+santoshfaction]
+
+
+
